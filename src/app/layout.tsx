880f7e2f33884da74e61325618aaa4fd34cf9b1a
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2025-03-30 22:18:59 +0530

    Added Home Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import { Metadata } from "next"
 import { ReactNode } from "react"
+import "../globals.css"
 
 interface RootLayoutProps {
     children: ReactNode
 }
 
 export const metadata: Metadata = {
-    title: "Project Nexus"
+    title: "Project Nexus",
+    icons: {
+        icon: "https://img.icons8.com/fluency/48/customer-insight.png"
+    }
 } satisfies Metadata;
 
 export default function RootLayout({ children }: RootLayoutProps) {
@@ -16,7 +20,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
-            <body>{ children }</body>
+            <body className="w-screen h-screen">{ children }</body>
         </html>
     )
 }
---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import HeaderElement from "@/components/HeaderElement";
+import {
+    GlobeAmericasIcon,
+    LockClosedIcon,
+    WrenchScrewdriverIcon
+} from "@heroicons/react/24/outline";
+
+export default function HomePage() {
+    return (
+        <>
+        <HeaderElement />
+        <main className="w-screen h-[calc(100vh-192px)] flex items-center justify-center flex-col gap-4">
+            <h1 className="text-2xl md:text-3xl lg:text-5xl tracking-wide text-center">
+                Tech for Tomorrow
+                <span className="text-emerald-600">.</span>
+            </h1>
+            <div className="flex items-center justify-center gap-2">
+                <GlobeAmericasIcon className="size-6 hover:text-indigo-800" title="Universality"/>
+                <LockClosedIcon className="size-6 hover:text-red-800" title="Security" />                
+                <WrenchScrewdriverIcon className="size-6 hover:text-blue-800" title="Customizability"/>
+            </div>
+        </main>
+        </>
+    );
+}
